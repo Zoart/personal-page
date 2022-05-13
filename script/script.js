7d8832143b9d538459bfadcc563ad6329467fd29
Oleg <neperate@gmail.com>
2022-05-13 14:30:00 +0300

    new

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 const page = document.getElementById("page");
 const themeButton = document.getElementById("light");
 const cards_github = document.getElementsByClassName('cards__githab')
+const projects_button = document.querySelector('.projects-button');
 
 themeButton.addEventListener('click', () => {
   // page.classList.toggle("dark-theme");
@@ -9,8 +10,15 @@ themeButton.addEventListener('click', () => {
   if (class_list.contains('white-theme'))
   {
     class_list.replace('white-theme', 'dark-theme');
+
     themeButton.style.boxShadow = "0 -6px 9px white, 0 6px 9px white, 6px 0 9px white, -6px 0 9px white"
     themeButton.style.backgroundColor = 'white';
+
+    if (projects_button) {
+      projects_button.style.backgroundColor = 'white';
+      projects_button.style.color = 'black';
+      projects_button.style.boxShadow = '0 0 10px 3px white';
+    }
     for (let i = 0; i<cards_github.length; i++)
     {
      cards_github.style.backgroundColor = 'white';
@@ -19,8 +27,16 @@ themeButton.addEventListener('click', () => {
   else
   {
     class_list.replace('dark-theme', 'white-theme');
+
     themeButton.style.boxShadow = "0 -6px 9px black, 0 6px 9px black, 6px 0 9px black, -6px 0 9px black"
     themeButton.style.backgroundColor = 'black';
+
+    if (projects_button) {
+      projects_button.style.backgroundColor = 'black';
+      projects_button.style.color = 'white';
+      projects_button.style.boxShadow = '0 0 10px 3px black';
+    }
+
     for (let i = 0; i<cards_github.length; i++)
     {
      cards_github.style.backgroundColor = 'black';
